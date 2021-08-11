32f9fd66e71dbfc9b0c72c04f5b10b8c01de517b
hideyukikanazawa <kanazawahideyuki@gmail.com>
2021-08-11 16:09:39 +0800

    Updated solution

--- palindrone/is_palindrone.py
+++ palindrone/is_palindrone.py
@@ -6,26 +6,53 @@ def is_palindrone(s: str) -> bool:
     return True
 
 
+# def longest_palindrone(s: str) -> str:
+#     res = ""
+#     for i in range(len(s) - 1):
+#         step, cur = 1, s[i]
+#         while is_palindrone(cur) and i - step >= 0 and i + step < len(s) - 1:
+#             if len(cur) > len(res):
+#                 res = cur
+#             cur = s[i-step] + cur + s[i+step]
+#             step += 1
+
+#         step, cur = 1, ""
+#         while is_palindrone(cur) and i - step >= -1 and i + step < len(s) - 1:
+#             if len(cur) > len(res):
+#                 res = cur
+#             cur = s[i] + cur + s[i+step]
+#             step += 1
+        
+#     return res
+
+
 def longest_palindrone(s: str) -> str:
-    res = ""
+    if not s: return ""
+    if len(s) == 1: return s
+
+    res = s[0]
     for i in range(len(s) - 1):
         step, cur = 1, s[i]
-        while is_palindrone(cur) and i - step >= 0 and i + step < len(s) - 1:
-            if len(cur) > len(res):
-                res = cur
+        while i - step >= 0 and (i + step) <= (len(s) - 1):
             cur = s[i-step] + cur + s[i+step]
             step += 1
-
-        step, cur = 1, ""
-        while is_palindrone(cur) and i - step >= 0 and i + step < len(s) - 1:
+            if not is_palindrone(cur):
+                break
             if len(cur) > len(res):
                 res = cur
-            cur = s[i] + cur + s[i+step]
+
+        step, cur = 0, ""
+        while i - step >= 0 and (i + step) <= (len(s) - 1):
+            cur = s[i-step] + cur + s[i+step+1]
             step += 1
+            if not is_palindrone(cur):
+                break
+            if len(cur) > len(res):
+                res = cur
         
     return res
 
 
-# print(longest_palindrone("avkesekjhkj"))
+print(longest_palindrone("avkesekjhkj"))
 print(longest_palindrone("foobar"))
 print(longest_palindrone("ovo"))
